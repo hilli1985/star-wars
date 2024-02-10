2c925ed4dec2e49232115bc32e71c9aafcd075d3
hilli1985 <hillameir85@gmail.com>
2024-02-11 00:40:22 +0200

    *add integration test

--- server/tests/index.test.ts
+++ server/tests/index.test.ts
@@ -18,3 +18,23 @@ describe('GET /api',  () => {
     expect(response.text).toBe('API main routes');
   });
 });
+
+// integration test
+describe('GET /api',  () => {
+  it('should return api basic route 2', async () => {
+    const response = await request(app).get('/api/character?search=lu');
+    const json1 = JSON.stringify(    [
+      { name: 'Luke Skywalker', height: '172', mass: '77', gender: 'male' },
+      {
+        name: 'Luminara Unduli',
+        height: '170',
+        mass: '56.2',
+        gender: 'female'
+      }
+    ]);
+    const json2 = JSON.stringify(response.body.results);
+    expect(response.status).toBe(200);
+    expect(response.body.results.length).toBe(2);
+    expect(json1).toBe(json2);
+  });
+});
